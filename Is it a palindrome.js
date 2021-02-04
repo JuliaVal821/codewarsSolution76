a0739d07ea5fc1e4e607618b2e6e096307ff4ac8
JuliaVal821 <julia.kolgatina@gmail.com>
2021-02-03 23:28:59 -0500

    Wrote function isPalindrome that checks if a given string (case insensitive) is a palindrome.

---
+++ Is it a palindrome.js
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/javascript
+
+    function isPalindrome(x) {
+        let str = '';
+        x = x.toLowerCase();
+        for(let i = 0; i < x.length; i++){
+            str = x[i] + str;
+        }
+        return str === x;
+    }
+
